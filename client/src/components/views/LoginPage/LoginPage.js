9b17d851073b5583519bfc86f1a9cbace4823548
seonu1204 <seonu7222@daum.net>
2022-11-08 02:56:03 +0900

    Project

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import Axios from 'axios'
+import {useDispatch} from 'react-redux'
+import { loginUser } from '../../../_actions/user_action';
+import {useNavigate} from 'react-router-dom';
+import Auth from '../../../hoc/auth';
+
+function LoginPage() {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();  // 페이지 리프레쉬를 막기위해
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+  
+    dispatch(loginUser(body))
+    .then(response => {
+      if(response.payload.loginSuccess){
+        navigate('/')
+      } else{
+        alert('Error')
+      }
+    })
+  }
+
+  return (
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center'
+      , width: '100%', height: '100vh'
+  }}>
+
+      <form style={{ display: 'flex', flexDirection: 'column'}}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type = "email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type = "password" value={Password} onChange={onPasswordHandler} />
+
+        <br />
+        <button type="submit">
+          Login
+        </button>
+      </form>
+
+
+    </div>
+  )
+}
+
+export default Auth(LoginPage, false);
